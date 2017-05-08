a5b3195c8140bcb9394dd85bf02873db696152c4
cptCrunchy <jasong84@gmail.com>
2017-05-08 10:32:56 -0500

    further practical use of Java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by cpt_crunch on 5/8/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Pinnacles!");
+    }
+}
+
+
+    
--- src/helloWorld.java
+++
@@ -1,7 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * Created by cpt_crunch on 5/8/17.
- */
-public class helloWorld {
-}
