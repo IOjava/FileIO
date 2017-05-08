25769a2dea87aa1bcb6085d85779ed170f1eb54c
cptCrunchy <jasong84@gmail.com>
2017-05-08 10:42:01 -0500

    renamed Java file to HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
  * Created by cpt_crunch on 5/8/17.
  */
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Pinnacles!");
     }
