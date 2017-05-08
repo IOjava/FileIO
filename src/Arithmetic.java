a8943f210c74bccfd646963e0d25ec0036daed77
cptCrunchy <jasong84@gmail.com>
2017-05-08 13:42:23 -0500

    completed exercises with arithmetic

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -4,7 +4,7 @@ public class Arithmetic {
   public static void main(String[] args){
     int myFavoriteNumber = 21;
     double myString = 3.14159;
-    long myNumber = 123456;
+    float myNumber = 123;
 
     System.out.println(myFavoriteNumber);
     System.out.println(myString);
