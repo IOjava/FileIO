e225492b43ecb7080ab19956ff804142c7b2e2a8
cptCrunchy <jasong84@gmail.com>
2017-05-25 14:01:28 -0500

    Completed java File IO exercise

---
+++ src/io/File_IO.java
@@ -0,0 +1,28 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class File_IO {
+    public static void main(String[] args) throws IOException{
+        String directory = "data";
+        String filename = "info.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)){
+            Files.createDirectories(dataDirectory);
+        }
+        if(! Files.exists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+        List<String> lines = Files.readAllLines(dataFile);
+        for(String line : lines){
+            System.out.println(line);
+        }
+    }
+}
